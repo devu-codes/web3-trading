eaa675fcc7a15809f9522570997e2fbed2b0bf97
devu-codes <devanshpandey977@gmail.com>
2025-05-17 21:24:33 +0530

    Token Approval

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,11 +11,18 @@ contract Token {
 
     // Track balances
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance; 
 
     event Transfer(
         address indexed _from,
         address indexed _to,
         uint256 _value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
     );
 
     constructor(
@@ -41,6 +48,20 @@ contract Token {
         balanceOf[_to] = balanceOf[_to] + _value;
         
         emit Transfer(msg.sender, _to, _value);
+        
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_spender != address(0), "Invalid address");
+        
+        
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
         
         return true;
     }
